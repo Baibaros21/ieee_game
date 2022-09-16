b0d08224e074791fbb15bfa55ff46349263aed52
Baibaros21 <alaatarek20@gmail.com>
2022-09-16 21:21:11 +0400

    final

--- script.js
+++ script.js
@@ -11,18 +11,43 @@ userAns = $("#answer").val().toLowerCase();
 switch(question){
     case "1":
         if(userAns!="Institute of Electrical and Electronics Engineers".toLowerCase()){
-            $("#answer").css("border-width","2");
-            $("#answer").css("border-color","red");
-            $(objClass).text("wrong?! maybe try this one?"); 
-            $("#nextTip").hide();
+            wrong(objClass);
            
         }else{
-            $("#answer").css("border-width","2");
-            $("#answer").css("border-color","green");
-            $(objClass).text("Hurray!!"); 
-            $("#nextTip").show();
+            right(objClass);   
         }
+        break; 
+
+        case "2":
+            if(userAns!="accelerometer".toLowerCase()){
+                wrong(objClass);
+               
+            }else{
+                right(objClass);   
+            }
+        case "3":
+            if(userAns!="compiler".toLowerCase()){
+                wrong(objClass);
+               
+            }else{
+                right(objClass);   
+            }
+
 
 }
 
+    }
+
+    function wrong(objClass){
+        $("#answer").css("border-width","2");
+        $("#answer").css("border-color","red");
+        $(objClass).text("wrong?! maybe try this one?"); 
+        $("#nextTip").hide();
+    }
+
+    function right(objClass){
+        $("#answer").css("border-width","2");
+        $("#answer").css("border-color","green");
+        $(objClass).text("Hurray!!"); 
+        $("#nextTip").show();
     }
